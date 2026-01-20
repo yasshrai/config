2b11ba7c43716bf2908101b8bd1263677e62b90f
Yash Rai <yash2154rai@gmail.com>
2026-01-20 22:19:53 +0530

    update neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,24 @@
--- ========================
--- Basic settings
--- ========================
+-- Basic settings for lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- relativenumber
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- ========================
 -- Terminal setup
--- ========================
-
 -- Open terminal in insert mode
 vim.api.nvim_create_autocmd("TermOpen", {
   pattern = "*",
@@ -22,18 +33,32 @@ vim.api.nvim_create_user_command("Term", function()
   vim.cmd("botright split | terminal")
 end, {})
 
--- ========================
 -- Resize splits easily
--- ========================
 vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { silent = true })
 vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { silent = true })
 vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { silent = true })
 vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { silent = true })
 
--- ========================
 -- Optional: mouse resize
--- ========================
 vim.opt.mouse = "a"
 
+-- ColorScheme
 vim.cmd("colorscheme slate")
+
+-- Tab 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+
+require("config.lazy")
 
---
+++ nvim/lua/config/lazy.lua
@@ -0,0 +1,11 @@
+
+require("lazy").setup({
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({})
+    end,
+  },
+})
+
